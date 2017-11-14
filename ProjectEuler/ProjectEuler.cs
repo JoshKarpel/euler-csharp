389b5169165115f1bce6249c29a0cc4e3ffde56b
JoshKarpel <josh.karpel@gmail.com>
2017-11-13 23:05:48 -0600

    Solved problem 001.

---
+++ ProjectEuler/ProjectEuler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ProjectEuler.solvers;
+
+namespace ProjectEuler {
+    internal class Solver {
+        public static void Main(string[] args) {
+            Dictionary<string, Func<UInt64>> solver_dictionary = GetSolverDictionary();
+
+            Func<UInt64> solver;
+            if (!solver_dictionary.TryGetValue(args[0], out solver)) {
+                string not_found_msg = String.Format("Couldn't find a solver for {0}", args[0]);
+                Console.WriteLine(not_found_msg);
+                System.Environment.Exit(0);
+            }
+            UInt64 answer = solver();
+
+            string solved_msg = String.Format("Answer: {0}", answer);
+            Console.WriteLine(solved_msg);
+        }
+
+        public static Dictionary<string, Func<UInt64>> GetSolverDictionary() {
+            Dictionary<string, Func<UInt64>> solver_dictionary = new Dictionary<string, Func<UInt64>>();
+
+            solver_dictionary.Add("001", solvers.p001.solve);
+
+            return solver_dictionary;
+        }
+    }
+}
---
+++ ProjectEuler/solvers/p001.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjectEuler.solvers {
+    public class p001 {
+        public static UInt64 solve() {
+            UInt64 acc = 0;
+            for (UInt64 n = 0; n < 1000; n++) {
+                if (n % 3 == 0 || n % 5 == 0) {
+                    acc += n;
+                }
+            }
+
+            return acc;
+        }
+    }
+}
