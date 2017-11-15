3f0e688019a4303d4cc36e1bfd9ed4497abf84e3
JoshKarpel <josh.karpel@gmail.com>
2017-11-14 20:51:27 -0600

    Fixed some coding style issues.

--- ProjectEuler/ProjectEuler.cs
+++ ProjectEuler/ProjectEuler.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-using ProjectEuler.solvers;
-
 namespace ProjectEuler {
-    internal class Solver {
+    internal static class ProjectEuler {
         public static void Main(string[] args) {
-            Dictionary<string, Func<UInt64>> solver_dictionary = GetSolverDictionary();
+            var solver_dictionary = GetSolverDictionary();
 
-            Func<UInt64> solver;
+            Func<long> solver;
             if (!solver_dictionary.TryGetValue(args[0], out solver)) {
                 string not_found_msg = String.Format("Couldn't find a solver for {0}", args[0]);
                 Console.WriteLine(not_found_msg);
                 System.Environment.Exit(0);
             }
-            UInt64 answer = solver();
+            var answer = solver();
 
-            string solved_msg = String.Format("Answer: {0}", answer);
+            var solved_msg = String.Format("Answer: {0}", answer);
             Console.WriteLine(solved_msg);
         }
 
-        public static Dictionary<string, Func<UInt64>> GetSolverDictionary() {
-            Dictionary<string, Func<UInt64>> solver_dictionary = new Dictionary<string, Func<UInt64>>();
+        public static Dictionary<string, Func<long>> GetSolverDictionary() {
+            var solver_dictionary = new Dictionary<string, Func<long>>();
 
-            solver_dictionary.Add("001", solvers.p001.solve);
+            solver_dictionary.Add("001", solvers.P001.Solve);
 
             return solver_dictionary;
         }
--- ProjectEuler/solvers/P001.cs
+++ ProjectEuler/solvers/P001.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
+
 namespace ProjectEuler.solvers {
-    public class p001 {
-        public static UInt64 solve() {
-            UInt64 acc = 0;
-            for (UInt64 n = 0; n < 1000; n++) {
+    public static class P001 {
+        public static long Solve() {
+            long acc = 0;
+            for (long n = 0; n < 1000; n++) {
                 if (n % 3 == 0 || n % 5 == 0) {
                     acc += n;
                 }
